fa1ae61ce4123ee1d7cd0ac5819d9707eb3363a8
Joaquin Bru <brujoaco@gmail.com>
2021-09-15 19:31:33 -0300

    Terminando

--- script/mainPrueba.js
+++ script/mainPrueba.js
@@ -7,12 +7,6 @@ const router = () => {
   app.listarCervezas('#menu_container-cervezas');
   app.listarTragos('#menu_container-tragos');
 
-  
-}
-
-$(window).on('load', () => {
-  router();
-
   $("#toggleOn").click(() => {
     $("#ventana_carrito").animate({width: "40%"});
     $(".carrito_total").show();
@@ -48,34 +42,71 @@ $(window).on('load', () => {
   });
 
   //Evento para que se abra el formulario
-$("#comprarForm").on("click", (e) => {
-  e.preventDefault();
-  if(PEDIDO.length > 0){
-    //Validacion de que esten todos los campos completos
-    if($("#nombre").val() == "" || $("#direccion").val() == "" || $("#telefono").val() == ""){
-      alert("Complete todos los datos");
+  $("#comprarForm").on("click", (e) => {
+    e.preventDefault();
+    if(PEDIDO.length > 0){
+      //Validacion de que esten todos los campos completos
+      if($("#nombre").val() == "" || $("#direccion").val() == "" || $("#telefono").val() == ""){
+        alert("Complete todos los datos");
+      } else {
+        //Capturo la informacion del formulario y el pedido
+        let info = {
+          nombre: $("#nombre").val(),
+          direccion: $("#direccion").val(),
+          telefono: $("#telefono").val(),
+          pedido: PEDIDO
+        };
+
+        localStorage.setItem("UltimoPEDIDO", JSON.stringify(info));
+    
+        //Envio los datos a la api
+        $.ajax({
+          method: "POST",
+          url: APIURL,
+          data: info,
+          success: (res) => {
+            alert("Pedido exitoso!");
+            console.log(res);
+          }
+        });
+      };    
     } else {
-      //Capturo la informacion del formulario y el pedido
-      let info = {
-        nombre: $("#nombre").val(),
-        direccion: $("#direccion").val(),
-        telefono: $("#telefono").val(),
-        pedido: PEDIDO
-      };
-  
-      //Envio los datos a la api
-      $.ajax({
-        method: "POST",
-        url: APIURL,
-        data: info,
-        success: (res) => {
-          alert("Pedido exitoso!");
-          console.log(res);
-        }
-      });
-    };    
-  } else {
-    alert("Su pedido esta vacio!");
-  };
-});
+      alert("Su pedido esta vacio!");
+    };
+  });
+
+  $("#repetirBtn").on("click", () => {
+    let info = JSON.parse(localStorage.getItem("UltimoPEDIDO"));
+    $("#tabla").empty();
+    $("#nombre").val(info.nombre);
+    $("#direccion").val(info.direccion);
+    $("#telefono").val(info.telefono);
+    let total = 0;
+
+    //Agrego un detalle del pedido en el formulario
+    info.pedido.forEach(item => {
+      $("#tabla").append(`<tr>
+                            <th>${item.nombre}</th>
+                            <th>x${item.cantidad}</th>
+                            <th>$${item.precio * item.cantidad}</th>
+                          </tr>`);
+      total += item.precio * item.cantidad;
+    });
+    $("#tabla").append(`<tr>
+                            <th>TOTAL</th>
+                            <th></th>
+                            <th>$${total}</th>
+                          </tr>`);
+  });
+
+  $("#vaciarBtn").on("click", () => {
+    PEDIDO.length = 0;
+    Pedido.refreshPedido();
+    Pedido.refreshBtn();
+    Pedido.refreshTotal();
+  });
+};
+
+$(window).on('load', () => {
+  router();
 })
