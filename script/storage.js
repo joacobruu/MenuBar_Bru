bf50adbe8b454fa6a9990ae4c4eb24ef1a3e3b31
Joaquin Bru <brujoaco@gmail.com>
2021-09-15 20:02:35 -0300

    detalles

--- script/storage.js
+++ script/storage.js
@@ -1,9 +1,9 @@
 const URLJSON = '../storage/productos.json';
 
 const PRODUCTOS = $.getJSON(URLJSON, (res, estado) => {
-if(estado == 'success') {
-  localStorage.setItem("PRODUCTOS", JSON.stringify(res))
-} else {
-  console.log(estado);
-};
+  if(estado == 'success') {
+    localStorage.setItem("PRODUCTOS", JSON.stringify(res))
+  } else {
+    console.log(estado);
+  };
 });
